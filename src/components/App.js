5104dedff3d689a19039f97d401b8b44746e021c
Rashmi-Salunkhe <158038788+Rashmi-Salunkhe@users.noreply.github.com>
2024-10-03 21:41:38 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,48 @@
-
-import React from "react";
-import './../styles/App.css';
+import React from 'react';
+import { Provider, useDispatch, useSelector } from 'react-redux';
+import store from './store';
+import { updateName, updateEmail } from './action';
 
 const App = () => {
+  const dispatch = useDispatch();
+  const { name, email } = useSelector((state) => state);
+
+  const handleNameChange = (e) => {
+    dispatch(updateName(e.target.value));
+  };
+
+  const handleEmailChange = (e) => {
+    dispatch(updateEmail(e.target.value));
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>User Information</h1>
+      <div>
+        <label>
+          Name:
+          <input type="text" value={name} onChange={handleNameChange} placeholder="Enter name" />
+        </label>
+      </div>
+      <div>
+        <label>
+          Email:
+          <input type="email" value={email} onChange={handleEmailChange} placeholder="Enter email" />
+        </label>
+      </div>
+      <div>
+        <h2>Current values in store:</h2>
+        <p className="output">Name - {name}</p>
+        <p className="output">Email - {email}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+const AppWithProvider = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+export default AppWithProvider;
